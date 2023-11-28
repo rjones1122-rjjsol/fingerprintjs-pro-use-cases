72946c11ae3ee2c50705d773cd8aaf456e81f4ff
Juraj Uhlar <juro.uhlar@gmail.com>
2023-11-28 17:10:49 +0000

    test: force deploy

--- src/pages/api/admin/reset.ts
+++ src/pages/api/admin/reset.ts
@@ -56,6 +56,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
 async function tryToReset(req: NextApiRequest, res: NextApiResponse, ruleChecks: RuleCheck[]) {
   // Get requestId and visitorId from the client.
+  console.log('resetting');
 
   const { visitorId, requestId } = req.body as ResetRequest;
 
