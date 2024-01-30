341d84fcb6fc4ec30bb5508099c3e85355f6c500
Juraj Uhlar <juro.uhlar@gmail.com>
2024-01-30 19:50:22 +0000

    GTM test

--- src/client/DeploymentUtils.tsx
+++ src/client/DeploymentUtils.tsx
@@ -1,4 +1,22 @@
+import Script from 'next/script';
+
+const GTM_ID = 'GTM-NJH9B2SN';
+
 // Placeholder for deployment utilities, you can safely ignore, it's not relevant to the Fingerprint use case demos
 export default function DeploymentUtils() {
-  return null;
+  return (
+    <Script
+      id="gtag-base"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer', '${GTM_ID}');
+`,
+      }}
+    />
+  );
 }
